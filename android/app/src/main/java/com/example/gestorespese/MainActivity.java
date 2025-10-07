3539f4e6432dd282c26134997d1d41244160a188
Jerba Michol <jerbamichol@gmail.com>
2025-10-07 22:19:44 +0300

    Abilitato permesso microfono runtime per speech recognition

--- android/app/src/main/java/com/example/gestorespese/MainActivity.java
+++ android/app/src/main/java/com/example/gestorespese/MainActivity.java
@@ -1,5 +1,18 @@
 package com.example.gestorespese;
 
+import android.Manifest;
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Controlla permesso microfono
+        if (!this.hasPermission(Manifest.permission.RECORD_AUDIO)) {
+            this.requestPermission(Manifest.permission.RECORD_AUDIO, 100);
+        }
+    }
+}
