8e672d2fb291be82405a70c9dbe54de9bb897a6f
Jerba Michol <jerbamichol@gmail.com>
2025-10-11 20:35:53 +0200

    diag: apri Diagnostics come pagina iniziale

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,6 @@
-import React, { useState } from 'react';
-import NavBar from './components/NavBar.jsx';
-import Home from './pages/Home.jsx';
-import AddExpense from './pages/AddExpense.jsx';
-import Expenses from './pages/Expenses.jsx';
+import React from 'react';
+import Diagnostics from '@/pages/Diagnostics';
 
 export default function App() {
-  const [tab, setTab] = useState('home');
-  return (
-    <div style={{ maxWidth: 720, margin:'0 auto' }}>
-      <NavBar current={tab} onChange={setTab} />
-      {tab === 'home' && <Home />}
-      {tab === 'add' && <AddExpense onSaved={() => setTab('list')} />}
-      {tab === 'list' && <Expenses />}
-      <div style={{ height: 24 }} />
-    </div>
-  );
+  return <Diagnostics />;
 }
