96d3ac9a26d96142c97834346b7190a152410fd4
Jerba Michol <jerbamichol@gmail.com>
2025-10-11 21:48:38 +0200

    test(20251011-214601): banner UI, label 'Walleet TEST', debug .diag, CI bump

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,4 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
-
-// voce nativa (0), impronta shim (1)
-localStorage.voiceShimEnabled = '0';
-localStorage.biometricShimEnabled = '1';
-
 createRoot(document.getElementById('root')).render(<App />);
